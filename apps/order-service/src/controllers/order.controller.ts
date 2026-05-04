62c01cb624e6795a2bf6782da17330afa3913edd
Parzival Prime <dvnhrajput@gmail.com>
2026-05-04 22:29:45 +0530

    stripe apiversion update

--- apps/order-service/src/controllers/order.controller.ts
+++ apps/order-service/src/controllers/order.controller.ts
@@ -9,7 +9,7 @@ import { sendEmail } from "../utils/send-email";
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2026-01-28.clover"
+    apiVersion: "2026-02-25.clover"
 })
 
 export async function createPaymentIntent(req: any, res: Response, next: NextFunction) {
